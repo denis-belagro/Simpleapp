6199fc9eafac4cf0157fa7fb8ccf2fc5548b599c
Denis <dzianis.shcherba@gmail.com>
2021-10-11 20:19:19 +0300

    initial commit

---
+++ simpleapp/templatetags/custom_filters.py
@@ -0,0 +1,16 @@
+from django import template
+ 
+register = template.Library() # если мы не зарегестрируем наши фильтры, то django никогда не узнает где именно их искать и фильтры потеряются :(
+
+
+
+
+@register.filter(name='multiply')  
+def multiply(value, arg):
+    if isinstance(value, str) and isinstance(arg, int): # проверяем, что value — это точно строка, а arg — точно число, чтобы не возникло курьёзов
+        return str(value) * arg
+    else:
+        raise ValueError(f'Нельзя умножить {type(value)} на {type(arg)}')   
+        
+
+
