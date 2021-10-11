c6560b816f842fd33397856e2dd6565748e25980
Denis <dzianis.shcherba@gmail.com>
2021-10-11 22:45:55 +0300

    добавлены фильтра

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.core.paginator import Paginator # импортируем класс, позволяющий удобно осуществлять постраничный вывод
 from .models import Product
 from django.views.generic import ListView, DetailView # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
-from datetime import datetime
+#from datetime import datetime
+from .filters import ProductFilter # импортируем недавно написанный фильтр
 
 class ProductsList(ListView):
     model = Product
@@ -11,6 +12,12 @@ class ProductsList(ListView):
     context_object_name = 'products'
     ordering = ['-price']
     paginate_by = 1 # поставим постраничный вывод в один элемент
+
+    def get_context_data(self, **kwargs): # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса (привет, полиморфизм, мы скучали!!!)
+        context = super().get_context_data(**kwargs)
+        context['filter'] = ProductFilter(self.request.GET, queryset=self.get_queryset()) # вписываем наш фильтр в контекст
+        return context
+    
 
 # В отличие от дженериков, которые мы уже знаем, код здесь надо писать самому, переопределяя типы запросов (например, get- или post-)
 """
